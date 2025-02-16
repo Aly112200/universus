1be78a3dcb16965b723573f290b92e66af882ada
Alexandru Lucian Russ <russalexandru899@gmail.com>
2025-02-16 01:01:44 +0100

    Removed bootstrap import from main.jsx

--- src/vite.config.js
+++ src/vite.config.js
@@ -9,4 +9,9 @@ export default defineConfig({
     port: 5173,
   },
   base: '/', // Setarea pentru domeniu
+  build: {
+    rollupOptions: {
+      external: ['bootstrap'], // Evită problema de rezolvare a importului bootstrap
+    },
+  },
 });
